db0e58d61ea41f5a19626e9f7115cb6173292936
AryKno <36130832+AryKno@users.noreply.github.com>
2018-03-30 10:37:00 +0200

    Correction des erreurs Git

--- WebEditSci.pro
+++ WebEditSci.pro
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #-------------------------------------------------
 #
 # Project created by QtCreator 2016-01-26T21:17:42
@@ -36,42 +35,3 @@ RESOURCES += \
 #RC_FILE += \
  #   myapp.rc
 
-=======
-#-------------------------------------------------
-#
-# Project created by QtCreator 2016-01-26T21:17:42
-#
-#-------------------------------------------------
-# AUTHOR : Amaury KNOCKAERT
-
-QT       += core gui widgets
-
-LIBS += -lqscintilla2_qt5
-
-CONFIG += qscintilla2
-
-TARGET = WebEditSci
-TEMPLATE = app
-
-
-SOURCES += main.cpp\
-        mainwindow.cpp \
-    tabwidget.cpp \
-    editor.cpp \
-    textfile.cpp \
-    fileinfodialog.cpp
-
-HEADERS  += mainwindow.h \
-    tabwidget.h \
-    editor.h \
-    textfile.h \
-    fileinfodialog.h
-
-RESOURCES += \
-    res.qrc
-
-
-#RC_FILE += \
- #   myapp.rc
-
->>>>>>> refs/remotes/origin/master
--- fileinfodialog.cpp
+++ fileinfodialog.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /****************************************************************************/
 /****************************************************************************/
 /************************AUTHOR : AMAURY KNOCKAERT***************************/
@@ -54,60 +53,3 @@ void FileInfoDialog::setFile(QFileInfo *fileInfo)
     m_fileSize->setText("Taille : "+QString::number(m_fileInfo->size(),10)+" octets");
 }
 
-=======
-/****************************************************************************/
-/****************************************************************************/
-/************************AUTHOR : AMAURY KNOCKAERT***************************/
-/****************************************************************************/
-/****************************************************************************/
-/****************************************************************************/
-
-#include "fileinfodialog.h"
-
-FileInfoDialog::FileInfoDialog(QFileInfo *fileInfo, QWidget *parent) : QDialog(parent,Qt::WindowCloseButtonHint)
-{
-    m_fileInfo = new QFileInfo;
-    m_layout = new QGridLayout(this);
-    m_fileName = new QLabel(this);
-    m_fileSuffix = new QLabel(this);
-    m_filePath = new QLabel(this);
-    m_fileDateCreated = new QLabel(this);
-    m_fileDateModified = new QLabel(this);
-    m_fileSize = new QLabel(this);
-    m_fileDontExits = new QLabel("This file doens't exists yet");
-
-    if(fileInfo->exists())
-    {
-        m_layout->addWidget(m_fileName,0,0);
-        m_layout->addWidget(m_fileSuffix,1,0);
-        m_layout->addWidget(m_filePath,2,0);
-        m_layout->addWidget(m_fileDateCreated,3,0);
-        m_layout->addWidget(m_fileDateModified,4,0);
-        m_layout->addWidget(m_fileSize,5,0);
-        setFile(fileInfo);
-
-        setWindowTitle(fileInfo->fileName());
-    }else{
-        m_layout->addWidget(m_fileDontExits);
-    }
-        qDebug()<<"File info dialog created";
-}
-
-FileInfoDialog::~FileInfoDialog()
-{
-    //delete m_fileInfo;
-    qDebug()<<"File info dialog destroyed";
-}
-
-void FileInfoDialog::setFile(QFileInfo *fileInfo)
-{
-    m_fileInfo = fileInfo;
-    m_fileName->setText("Nom du fichier : "+m_fileInfo->fileName());
-    m_fileSuffix->setText("Type de fichier : "+m_fileInfo->completeSuffix());
-    m_filePath->setText("Chemin : "+m_fileInfo->filePath());
-    m_fileDateCreated->setText("Crée le : "+m_fileInfo->created().toString(Qt::DefaultLocaleLongDate));
-    m_fileDateModified->setText("Dernière modification le : "+m_fileInfo->lastModified().toString(Qt::DefaultLocaleLongDate));
-    m_fileSize->setText("Taille : "+QString::number(m_fileInfo->size(),10)+" octets");
-}
-
->>>>>>> refs/remotes/origin/master
